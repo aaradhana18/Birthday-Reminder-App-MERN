c2533646d42b9c6b7d95c72dd439b07f41031e76
aaradhana18 <madhumitamanivannane@gmail.com>
2023-01-20 22:49:20 +0530

    Mongo DB Connected

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("Hello World")
+import  express  from "express";
+import mongoose from "mongoose";
+
+const app = express()
+
+app.use("/api", (req,res)=>{
+    res.send("Welcome")
+})
+
+mongoose.set('strictQuery', true);
+
+mongoose
+  .connect(
+    "mongodb+srv://madhu18:9442463009@birthdayreminderapp.ge4fzq1.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => app.listen(5000))
+  .then(() =>
+    console.log("Connected TO Database and Listening TO Localhost 5000")
+  )
+  .catch((err) => console.log(err));
