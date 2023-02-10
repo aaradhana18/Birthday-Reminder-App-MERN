3f1dbe2902a6fb1025b16514da4a5156a48ac4a3
aaradhana18 <madhumitamanivannane@gmail.com>
2023-02-10 15:33:09 +0530

    Frontend (Data display)

---
+++ Client/bday-reminder/src/App.js
@@ -0,0 +1,68 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="app-title">BIRTHDAY REMINDER APP</div>
+      <div class="card mb-3">
+        <div class="row g-0">
+          <div class="col-md-4 rounded-start person-image">
+            <img src="..." class="img-fluid rounded-start" alt="#image"></img>
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">#Name1</h5>
+              <p class="card-text text-muted">#d.o.b1</p>
+              <p class="card-text">#description1.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <div class="row g-0">
+          <div class="col-md-4 rounded-start person-image">
+            <img src="..." class="img-fluid rounded-start" alt="#image"></img>
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">#Name2</h5>
+              <p class="card-text text-muted">#d.o.b2</p>
+              <p class="card-text">#description2.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <div class="row g-0">
+          <div class="col-md-4 rounded-start person-image">
+            <img src="..." class="img-fluid rounded-start" alt="#image"></img>
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">#Name2</h5>
+              <p class="card-text text-muted">#d.o.b2</p>
+              <p class="card-text">#description2.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <div class="row g-0">
+          <div class="col-md-4 rounded-start person-image">
+            <img src="..." class="img-fluid rounded-start" alt="#image"></img>
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">#Name2</h5>
+              <p class="card-text text-muted">#d.o.b2</p>
+              <p class="card-text">#description2.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
